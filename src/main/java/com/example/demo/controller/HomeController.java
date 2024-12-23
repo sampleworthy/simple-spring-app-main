24e527f5616056ca8b7de0f5b5480e8fa4a9c6da
Justin Laws <86179207+sampleworthy@users.noreply.github.com>
2024-12-24 02:55:12 +0800

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
     
     @GetMapping("/")
     public String home() {
-        return "This is the Default Landing Page for the Application Gateway";
+        return "This is the Default Landing Page for the Azure Front Door Demo";
     }
 }
